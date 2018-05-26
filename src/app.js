b60e2482c1e9bd476e0e4b50b603bb9bb80f4614
Tyler Confalone <tconfalone@gmail.com>
2018-05-26 11:11:30 -0700

    first

---
+++ src/app.js
@@ -0,0 +1,24 @@
+'use strict';
+
+let buffer = Buffer.from('../assets/bitmap.bmp');
+console.log(buffer);
+
+// let fs = require('fs');
+
+// let file = `${__dirname}/bitmap.bmp`;
+
+// console.log(buffer);
+
+// fs.readFile(file, (err, data) => {
+//   if (err) { throw err; }
+//   console.log(data);
+// });
+
+// let doSomethingWithTheContentsOfTheFile = (err, data) => {
+//   if (err) {
+//     throw err;
+//   }
+//   console.log(data);
+// };
+
+// fs.readFile(file, doSomethingWithTheContentsOfTheFile);
